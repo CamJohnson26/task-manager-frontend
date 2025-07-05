3ec770fec307f639bd046cd0bba0ca1f369d11c2
Cameron Johnson <cameronrossjohnson@gmail.com>
2025-07-04 20:38:11 -0500

    Fixed a few bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
 import './App.css'
 import {useHelloWorldApi} from "./taskManagerApi/useHelloWorldApi.ts";
-import {Auth0Provider} from "@auth0/auth0-react";
 import {LoginButton} from "./auth/LoginButton.tsx";
 import {LogoutButton} from "./auth/LogoutButton.tsx";
+import {Auth0ProviderWrapped} from "./auth/Auth0Provider.tsx";
 
 function App() {
   const {data, loading} = useHelloWorldApi()
 
   return (
-      <Auth0Provider
-        domain={import.meta.env.VITE_AUTH0_DOMAIN ?? ''}
-        clientId={import.meta.env.VITE_AUTH0_CLIENT_ID ?? ''}
-        authorizationParams={{
-          redirect_uri: window.location.origin
-        }}
-        useRefreshTokens={true}
-    >
+      <Auth0ProviderWrapped>
       {data}
       {loading && <div>Loading...</div>}
         <LoginButton /><LogoutButton />
-    </Auth0Provider>
+    </Auth0ProviderWrapped>
   )
 }
 
--- src/auth/Auth0Provider.tsx
+++ src/auth/Auth0Provider.tsx
@@ -8,7 +8,7 @@ export const Auth0ProviderWrapped = ({children}: PropsWithChildren) => {
         domain={import.meta.env.VITE_AUTH0_DOMAIN ?? ''}
         clientId={import.meta.env.VITE_AUTH0_CLIENT_ID ?? ''}
         authorizationParams={{
-            redirect_uri: window.location.origin
+            redirect_uri: window.location.href
         }}
     >
         {children}
